3f38b685c2c415841fe9ce7573ec0d9925618aa2
kernelk14 <khyleisaias@gmail.com>
2022-12-22 22:15:29 +0800

    PyBrain: Added external codes and documentation.
    I also added the ';' instruction that basically clears the stack.

---
+++ README.md
@@ -0,0 +1,19 @@
+# PyBrain
+A different take on BrainFudge (yk know what language do I mean here)
+
+## Quick Start
+
+Clone this repo then,
+
+if you are in Windows (which I'm not):
+```console
+C:\User\user> python main.py 
+```
+if you are in Linux/Unix:
+```console
+$ ./main.py
+```
+in Linux/Unix, i already made the code executable without calling python3.
+
+## Why did u did this?
+| Actually I don't know, maybe i'm bored.
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 from collections import deque
+from sys import argv
+
 # This is the program structure.
-# `-           K    K-3 = H     Y           Y-13 = L   L-7 = E
-program = "++++++++++,---,++++++++++++++,-------------,-------,/"
+# |-- The code below is being separated through a file.
+# `--           K    K-3 = H     Y           Y-13 = L   L-7 = E
+# program = "++++++++++,---,++++++++++++++,-------------,-------,/"
 # program = "++++++++++++++++++++++,--------,+-,/"
+prog = open(argv[1], 'r')
+program = prog.read().replace("\n", "")
 ip = 0
 
 alpha = "a"
-next_alpha = chr(ord(alpha) + 1)
 stack = deque()
 restore_stack = deque()
 while (ip < len(program)):
@@ -15,6 +19,7 @@ while (ip < len(program)):
     # print(code)
     stack.append(alpha)
     if (code == "+"):
+        next_alpha = chr(ord(alpha) + 1)
         stack.append(next_alpha)
         alpha = stack.pop()
         next_alpha = chr(ord(alpha) + 1)
@@ -33,10 +38,15 @@ while (ip < len(program)):
         restore_stack.append(a)
         stack.append(a)
     elif (code == "/"):
-        print("".join(restore_stack))
+        data = "".join(restore_stack)
+        print(data)
+    elif (code == ";"):
+        stack.clear()
+        restore_stack.clear()
+        alpha = "a"
     else:
         print(f"Position {ip}: Expecting `+`, `-`, `,` and `.`, but got `{code}`.")
         exit(1)
-    # print(restore_stack) 
-    # print(stack)
+    # print(f"The restore stack: {restore_stack}") 
+    # print(f"The stack: {stack}")
     ip += 1
